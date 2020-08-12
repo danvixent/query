f1f77ebba8525ea29c35eeead597a851c767f9aa
danvixent <danvixent@gmail.com>
2020-08-12 18:16:34 +0100

    add more cases to stringify

--- worker_funcs.go
+++ worker_funcs.go
@@ -1,7 +1,6 @@
 package query
 
 import (
-	"log"
 	"sort"
 	"strconv"
 )
@@ -171,6 +170,16 @@ func stringify(i interface{}) string {
 		return strconv.Itoa(int(*i.(*int8)))
 	case *int16:
 		return strconv.Itoa(int(*i.(*int16)))
+	case int:
+		return strconv.Itoa(i.(int))
+	case int64:
+		return strconv.FormatInt(i.(int64), 10)
+	case int8:
+		return strconv.Itoa(int(i.(int8)))
+	case int16:
+		return strconv.Itoa(int(i.(int16)))
+	case int32:
+		return strconv.FormatInt(int64(i.(int32)), 10)
 	case *uint:
 		return strconv.FormatUint(uint64(*i.(*uint)), 10)
 	case *uint8:
@@ -181,7 +190,16 @@ func stringify(i interface{}) string {
 		return strconv.FormatUint(uint64(*i.(*uint32)), 10)
 	case *uint64:
 		return strconv.FormatUint(uint64(*i.(*uint64)), 10)
+	case uint:
+		return strconv.FormatUint(uint64(i.(uint)), 10)
+	case uint8:
+		return strconv.FormatUint(uint64(i.(uint8)), 10)
+	case uint16:
+		return strconv.FormatUint(uint64(i.(uint16)), 10)
+	case uint32:
+		return strconv.FormatUint(uint64(i.(uint32)), 10)
+	case uint64:
+		return strconv.FormatUint(uint64(i.(uint64)), 10)
 	}
-	log.Printf("WARNING: non-pointer int,uint type passed to stringify")
 	return ""
 }
